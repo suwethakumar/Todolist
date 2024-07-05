21eb4ca8b2e9e214532ba5bb05cef2bf000b6bd9
SUWETHA K <161113549+suwethakumar@users.noreply.github.com>
2024-07-05 13:16:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const inputbox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+function addTask(){
+  if(inputbox.value ===''){
+    alert("you must write something!");
+  }  
+  else{
+    let li = document.createElement("li");
+    li.innerHTML=inputbox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML="\u00d7"
+    li.appendChild(span);
+  }
+  inputbox.value="";
+}
+listContainer.addEventListener("click",function(e){
+  if(e.target.tagName==="LI"){
+    e.target.classList.toggle("checked");
+  }
+  else if(e.target.tagName ==="SPAN"){
+    e.target.parentElement.remove();
+  }  
+},false);
+
